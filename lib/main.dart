f05143d60c3e67208162bf20ef0ed28e9d5a2f7e
C-Dev66 <90483221+C-Dev66@users.noreply.github.com>
2022-03-31 06:16:31 -0700

    Stage 5

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    FriendlyChatApp(),
+    const FriendlyChatApp(),
   );
 }
 
@@ -30,24 +30,37 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
   void _handleSubmitted(String text) {
-    _textController.clear(); }
-    @override
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(title: Text('FriendlyChat')),
-        body: _buildTextComposer(),
-      );
-    }
+    _textController.clear();
+  }
 
-    Widget _buildTextComposer() {
-      return Container(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('FriendlyChat')),
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
           margin: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: TextField(
-            controller: _textController,
-            onSubmitted: _handleSubmitted,
-            decoration:
-                const InputDecoration.collapsed(hintText: 'Send a message'),
-          ));
-    }
-  
+          child: Row(children: [
+            Flexible(
+                child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  const InputDecoration.collapsed(hintText: 'Send a message'),
+            )),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () => _handleSubmitted(_textController.text)),
+            )
+          ])),
+    );
+  }
 }
